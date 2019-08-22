a8c71ef043448b9e2c74e21b9d052b2f54999b21
zzz1998 <1440629746@qq.com>
2019-08-22 20:46:51 +0800

    完善线程

--- config/sidebar/thread/index.js
+++ config/sidebar/thread/index.js
@@ -4,7 +4,7 @@ const titles = ['进程与线程', 'Java线程', '补充'];
 
 const childrenData = [
     [''],
-    ['thread的创建', 'thread方法', 'thread同步', 'thread死锁', '线程池', '生产者消费者'],
+    ['thread的创建', 'thread方法', 'thread同步', 'thread死锁', 'thread通信','线程池', '生产者消费者'],
     ['sleep()、join()、yield()有什么区别', 'wait()、notify()、notifyAll()方法', 'thread问题']
 ];
 
