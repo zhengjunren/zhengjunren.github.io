6b931f841e067e1d514609f884f24811a042e2a3
zzz1998 <1440629746@qq.com>
2019-08-28 20:44:30 +0800

    deploy

--- config/sidebar/internet/index.js
+++ config/sidebar/internet/index.js
@@ -3,7 +3,7 @@ const sidebarUtils = require('../../../utils/sidebarUtils');
 const titles = ['计算机网络'];
 
 const childrenData = [
-    ['']
+    ['', '物理层']
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
