797e50d91344886f25cae313d8460ea07f1af17d
zzz1998 <1440629746@qq.com>
2019-08-29 22:49:36 +0800

    vuepress持续集成

--- config/sidebar/interview/index.js
+++ config/sidebar/interview/index.js
@@ -1,10 +1,11 @@
 const sidebarUtils = require('../../../utils/sidebarUtils');
 
-const titles = ['面试介绍', '基础篇'];
+const titles = ['面试介绍', 'JavaSE', 'JavaEE'];
 
 const childrenData = [
     [''],
-    ['basis/HTTP请求的GET与POST方式的区别', 'basis/httpstatus']
+    ['JavaSE/&和&&的区别'],
+    ['JavaEE/HTTP请求的GET与POST方式的区别', 'JavaEE/httpstatus']
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
--- config/sidebar/vuepress/index.js
+++ config/sidebar/vuepress/index.js
@@ -5,7 +5,7 @@ const titles = ['VuePress', '安装', '使用'];
 const childrenData = [
     [''],
     ['vuepress的安装'],
-    ['vuepress的初步使用', 'vuepress的使用', 'vuepress的配置', 'vuepress的部署']
+    ['vuepress的初步使用', 'vuepress的使用', 'vuepress的配置', 'vuepress的部署', '持续集成']
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
