556579a6457fff8f180116e133f10691480b3d20
zzz1998 <1440629746@qq.com>
2019-08-29 10:43:41 +0800

    test

--- config/sidebar/internet/index.js
+++
@@ -1,9 +0,0 @@
-const sidebarUtils = require('../../../utils/sidebarUtils');
-
-const titles = ['计算机网络'];
-
-const childrenData = [
-    ['', '物理层']
-];
-
-module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
