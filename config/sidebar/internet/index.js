d7ad49a504af9dc3fa31780e5163c877e31a333c
zzz1998 <1440629746@qq.com>
2019-08-31 21:22:20 +0800

    链接修改

--- config/sidebar/internet/index.js
+++ config/sidebar/internet/index.js
@@ -4,7 +4,7 @@ const titles = ['计算机网络', '重要概念'];
 
 const childrenData = [
     ['', '物理层'],
-    ['重要概念/', '重要概念/物理层']
+    ['重要概念/概述', '重要概念/物理层']
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
