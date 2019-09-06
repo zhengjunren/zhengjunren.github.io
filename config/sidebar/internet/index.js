12a2318700f65c77a5dbb8f392fd96b982323b58
zzz1998 <1440629746@qq.com>
2019-09-06 20:18:46 +0800

    网络层

--- config/sidebar/internet/index.js
+++ config/sidebar/internet/index.js
@@ -3,8 +3,8 @@ const sidebarUtils = require('../../../utils/sidebarUtils');
 const titles = ['计算机网络', '重要概念'];
 
 const childrenData = [
-    ['', '物理层', '数据链路层1', '数据链路层2'],
-    ['重要概念/概述', '重要概念/物理层', '重要概念/数据链路层']
+    ['', '物理层', '数据链路层1', '数据链路层2', '网络层'],
+    ['重要概念/概述', '重要概念/物理层', '重要概念/数据链路层',]
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
