c64fac4f1872176f7a6568b76778db78312727fe
zzz1998 <1440629746@qq.com>
2019-09-06 22:22:14 +0800

    更新面试问题

--- config/sidebar/interview/index.js
+++ config/sidebar/interview/index.js
@@ -4,8 +4,8 @@ const titles = ['面试介绍', 'JavaSE', 'JavaEE'];
 
 const childrenData = [
     [''],
-    ['JavaSE/&和&&的区别', 'JavaSE/try-catch-finally和return'],
-    ['JavaEE/HTTP请求的GET与POST方式的区别', 'JavaEE/httpstatus']
+    ['JavaSE/&和&&的区别', 'JavaSE/try-catch-finally和return', 'JavaSE/重载和重写的区别', 'JavaSE/类初始化顺序',],
+    ['JavaEE/HTTP请求的GET与POST方式的区别', 'JavaEE/httpstatus',]
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
