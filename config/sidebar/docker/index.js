64ebe3ab6aa7fe30ea678955af1276c306de695d
zzz1998 <1440629746@qq.com>
2019-10-10 23:50:53 +0800

    docker-compose网络设置

--- config/sidebar/docker/index.js
+++ config/sidebar/docker/index.js
@@ -8,7 +8,7 @@ const childrenData = [
     ['image/', 'image/获取镜像', 'image/列出镜像', 'image/删除本地镜像', 'image/使用Dockerfile定制镜像', 'image/Dockerfile指令'],
     ['container/', 'container/run', 'container/daemon', 'container/stop', 'container/attach_exec'],
     ['data-management/' ,'data-management/volume', 'data-management/bind-mounts'],
-    ['compose/', 'compose/install', 'compose/usage', 'compose/commands', 'compose/compose_file'],
+    ['compose/', 'compose/install', 'compose/usage', 'compose/commands', 'compose/compose_file','compose/net'],
     ['other/Docker常用命令',  'other/Docker命令查询', 'other/Docker-Compose常用命令']
 ];
 
