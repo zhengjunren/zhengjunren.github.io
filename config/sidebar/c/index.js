0dcbbd7baccbdc3020a8f031de878e33bb81cfbe
zzz1998 <1440629746@qq.com>
2020-08-09 19:52:55 +0800

    真题

--- config/sidebar/c/index.js
+++ config/sidebar/c/index.js
@@ -1,9 +1,10 @@
 const sidebarUtils = require('../../../utils/sidebarUtils');
 
-const titles = ['C语言', '示例代码'];
+const titles = ['C语言', '真题', '示例代码'];
 
 const childrenData = [
     ['', 'question', 'data-type', '优先级'],
+    ['test/2000'],
     ['case/']
 ];
 
