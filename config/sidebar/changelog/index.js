29c10927dcab8b59af4c8d2c6d76246a1f316f77
zzz1998 <1440629746@qq.com>
2020-09-18 10:41:07 +0800

    更新

--- config/sidebar/changelog/index.js
+++ config/sidebar/changelog/index.js
@@ -4,7 +4,7 @@ const titles = ['更新日志', '我的更新'];
 
 const childrenData = [
     [''],
-    ['2020-08', '2019-11', '2019-10', '2019-09', '2019-08'],
+    ['2020-09', '2020-08', '2019-11', '2019-10', '2019-09', '2019-08'],
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
