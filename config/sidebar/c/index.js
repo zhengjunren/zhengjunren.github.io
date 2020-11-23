cc863287193c489de849e75b0d1a697549cd2643
zzz1998 <1440629746@qq.com>
2020-11-23 15:41:23 +0800

    update

--- config/sidebar/c/index.js
+++ config/sidebar/c/index.js
@@ -16,7 +16,10 @@ const childrenData = [
         'question',
         'error-question-collections'
     ],
-    ['test/2000', 'test/2006', 'test/2007', 'test/2009', 'test/2010', 'test/2011'],
+    [
+        'test/2000', 'test/2006', 'test/2007', 'test/2009', 'test/2010', 'test/2011',
+        'test/2012', 'test/2013', 'test/2014', 'test/2015'
+    ],
     ['case/', 'case/case1', 'case/list1', 'case/list2', 'case/file1', 'case/file2']
 ];
 
