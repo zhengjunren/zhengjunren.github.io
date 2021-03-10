de6c0e61b6d3b962fc31c3bde291420871ec0fb2
zzz1998 <1440629746@qq.com>
2021-03-10 21:36:46 +0800

    update

--- config/sidebar/software_engineering/index.js
+++ config/sidebar/software_engineering/index.js
@@ -1,10 +1,11 @@
 const sidebarUtils = require('../../../utils/sidebarUtils');
 
-const titles = ['软件工程', '真题'];
+const titles = ['软件工程', '练习', '真题'];
 
 const childrenData = [
-    ['1', '2', '3', '4', '5', '6', '8', '10', '11', '12', '13'],
-    ['test/2015']
+    ['1', '2', '3', '4', '5', '6', '8', '10', '11', '12', '13', 'uml'],
+    ['exercise/综合1', 'exercise/简答题'],
+    ['test/2015', 'test/2016']
 ];
 
 module.exports = sidebarUtils.simpleGenSidebar(titles, childrenData);
