44bc04eaef339b7d56a071affb7a0b1999bfc8c4
zzz1998 <1440629746@qq.com>
2021-03-14 20:01:04 +0800

    update

--- config/sidebar/software_engineering/index.js
+++ config/sidebar/software_engineering/index.js
@@ -3,7 +3,7 @@ const sidebarUtils = require('../../../utils/sidebarUtils');
 const titles = ['软件工程', '练习', '真题'];
 
 const childrenData = [
-    ['1', '2', '3', '4', '5', '6', '8', '10', '11', '12', '13', 'uml'],
+    ['1', '2', '3', '4', '5', '6', '7', '8', '10', '11', '12', '13', 'uml'],
     ['exercise/综合1', 'exercise/简答题'],
     ['test/2015', 'test/2016', 'test/2017', 'test/2018', 'test/2019']
 ];
